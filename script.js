4424e8f93e79714c22d9a2219f4eb2232b353204
Daniel Redondo Velasco <danielredondovelasco@gmail.com>
2025-02-26 20:36:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Función para volver a la página principal
+function goToHomePage() {
+    window.location.href = 'index.html';
+}
+
+// Función para volver arriba
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
